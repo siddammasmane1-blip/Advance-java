e88cc267175e6532149ae2928c13444bcd134f0f
siddammasmane1-blip <siddammasmane1@gmail.com>
2026-04-21 22:01:29 +0530

    Update p2.java

--- p2.java
+++ p2.java
@@ -1,3 +1,6 @@
+/*Write a java program for getting different colors through ArrayList interface and remove the
+2nd element and color "Blue" from the ArrayList*/
+
 package Swings;
 
 import java.util.*;

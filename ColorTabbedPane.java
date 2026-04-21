66a48391d605b52b539a4f6f136b6d37663a6c54
siddammasmane1-blip <siddammasmane1@gmail.com>
2026-04-21 22:12:48 +0530

    Add files via upload

---
+++ ColorTabbedPane.java
@@ -0,0 +1,54 @@
+package Swings;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+
+public class ColorTabbedPane extends JFrame {
+
+    private JTabbedPane tabbedPane;
+
+    public ColorTabbedPane() {
+
+        // Create TabbedPane
+        tabbedPane = new JTabbedPane();
+
+        // Create panels with different background colors
+        JPanel cyanPanel = new JPanel();
+        cyanPanel.setBackground(Color.CYAN);
+
+        JPanel magentaPanel = new JPanel();
+        magentaPanel.setBackground(Color.MAGENTA);
+
+        JPanel yellowPanel = new JPanel();
+        yellowPanel.setBackground(Color.YELLOW);
+
+        // Add tabs
+        tabbedPane.addTab("Cyan", cyanPanel);
+        tabbedPane.addTab("Magenta", magentaPanel);
+        tabbedPane.addTab("Yellow", yellowPanel);
+
+        // Add ChangeListener to detect tab selection
+        tabbedPane.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                int selectedIndex = tabbedPane.getSelectedIndex();
+                String selectedTitle = tabbedPane.getTitleAt(selectedIndex);
+                System.out.println("Selected Color is: " + selectedTitle);
+            }
+        });
+
+        add(tabbedPane);
+
+        setTitle("Color Tabbed Pane");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ColorTabbedPane();
+    }
+}

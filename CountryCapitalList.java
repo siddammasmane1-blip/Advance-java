b924fd3b15ccaf03f429681bdd713a89996387b6
siddammasmane1-blip <siddammasmane1@gmail.com>
2026-04-21 22:06:26 +0530

    Add files via upload

---
+++ CountryCapitalList.java
@@ -0,0 +1,78 @@
+package Swings;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class CountryCapitalList extends JFrame {
+
+    private JList<String> countryList;
+    private Map<String, String> capitalMap;
+
+    public CountryCapitalList() {
+
+        // Create capital mapping
+        capitalMap = new HashMap<>();
+        capitalMap.put("USA", "Washington D.C.");
+        capitalMap.put("India", "New Delhi");
+        capitalMap.put("Vietnam", "Hanoi");
+        capitalMap.put("Canada", "Ottawa");
+        capitalMap.put("Denmark", "Copenhagen");
+        capitalMap.put("France", "Paris");
+        capitalMap.put("Great Britain", "London");
+        capitalMap.put("Japan", "Tokyo");
+        capitalMap.put("Africa", "Addis Ababa");  // (African Union Capital)
+        capitalMap.put("Greenland", "Nuuk");
+        capitalMap.put("Singapore", "Singapore");
+
+        // Create list model and add countries
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        listModel.addElement("USA");
+        listModel.addElement("India");
+        listModel.addElement("Vietnam");
+        listModel.addElement("Canada");
+        listModel.addElement("Denmark");
+        listModel.addElement("France");
+        listModel.addElement("Great Britain");
+        listModel.addElement("Japan");
+        listModel.addElement("Africa");
+        listModel.addElement("Greenland");
+        listModel.addElement("Singapore");
+
+        // Create JList
+        countryList = new JList<>(listModel);
+
+        // Add Selection Listener
+        countryList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+
+                if (!e.getValueIsAdjusting()) {
+                    String selectedCountry = countryList.getSelectedValue();
+
+                    if (selectedCountry != null) {
+                        String capital = capitalMap.get(selectedCountry);
+                        System.out.println("Capital of " + selectedCountry + " is: " + capital);
+                    }
+                }
+            }
+        });
+
+        add(new JScrollPane(countryList));
+
+        setTitle("Country and Capital List");
+        setSize(300, 250);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new CountryCapitalList();
+    }
+}

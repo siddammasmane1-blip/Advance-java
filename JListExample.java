aff4d2fd4a2d2609ddce995f06718a6ca3ad2ec2
siddammasmane1-blip <siddammasmane1@gmail.com>
2026-04-21 22:21:12 +0530

    Update JListExample.java

--- JListExample.java
+++ JListExample.java
@@ -1,3 +1,9 @@
+/*Develop a Swing program in Java to add the countries USA, India, Vietnam, Canada, Denmark,
+France, Great Britain, Japan, Africa, Greenland, Singapore into a JList and display them on console
+whenever the countries are selected on the list.*/
+
+
+
 package Swings;
 import java.util.List;
 import javax.swing.DefaultListModel;

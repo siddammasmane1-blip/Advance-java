23dc5086611938b373689c5b1e561ff98dd0af82
siddamma <siddammasmane1@gmail.com>
2026-04-26 10:51:04 +0530

    commit

---
+++ src/main/java/servlet/FactorialServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@SuppressWarnings("serial")
+@WebServlet("/factorial")   // Annotation replaces web.xml
+public class FactorialServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        int num = Integer.parseInt(request.getParameter("num"));
+        long fact = 1;
+
+        for(int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+
+        out.println("<html><body>");
+        out.println("<h2>Factorial of " + num + " is: " + fact + "</h2>");
+        out.println("</body></html>");
+    }
+}
